4252ed64bbc8417f15f9aa28801bafb5ed2da41b
busrainceler <busra.inceler@identify.com.tr>
2023-12-03 16:59:35 +0300

    template struct example

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -9,23 +8,28 @@ import (
 func main() {
 	//http.Handle("/", http.FileServer(http.Dir(".")))
 	http.HandleFunc("/", Anasayfa)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/detay", Detay)
+	http.ListenAndServe(":9090", nil)
 }
 
 func Anasayfa(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Merhaba Dünya")
-	view, _ := template.ParseFiles("index.html")
-	//data := "Go'dan gelen veri..."
+	view, _ := template.ParseFiles("index.html", "navbar.html")
+	data := "Go'dan gelen veri..."
 
-	data := make(map[string]interface{})
+	/*data := make(map[string]interface{})
 	data["sayilar"] = []int{1, 2, 3, 4, 5}
 	data["is_admin"] = false
 	data["sayi"] = 10
-	/* data := Data{
+	data := Data{
 		Veri:    "Büşra",
 		Sayilar: []int{1, 2, 3, 4},
 	} */
-	view.Execute(w, data)
+	view.ExecuteTemplate(w, "anasayfa", data)
+}
+
+func Detay(w http.ResponseWriter, r *http.Request) {
+	view, _ := template.ParseFiles("detay.html", "navbar.html")
+	view.ExecuteTemplate(w, "detay", nil)
 }
 
 /* type Data struct {
